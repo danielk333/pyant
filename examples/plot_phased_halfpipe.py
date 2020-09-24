bugfixes, peak gain scaling, merge-fixes

examples/plot_phased_halfpipe.py
@@ -14,7 +14,7 @@ beam = pyant.PhasedFiniteCylindricalParabola(
     elevation=90.0, 
     depth=18,
     phase_steering = [5.0,10.0,25.0,30.0],
-    frequency=224.0e6,
+    frequency=30e6,
     width=120.0,
     height=40.0,
 )
@@ -47,16 +47,23 @@ beam_two = pyant.PhasedFiniteCylindricalParabola(
 fig, axes = plt.subplots(2,3,figsize=(10,6),dpi=80)
 for i in range(beam_two.named_shape()['phase_steering']):
     for j in range(beam_two.named_shape()['pointing']):
+        ind = {
+            "phase_steering":i,
+            "pointing":j,
+        }
+
         pyant.plotting.gain_heatmap(
             beam_two, 
             resolution=901, 
             min_elevation=60.0, 
             ax=axes[i,j],
-            ind = {
-                "phase_steering":i,
-                "pointing":j,
-            },
+            ind = ind,
         )
-        axes[i,j].set_title(f'{int(beam_two.phase_steering[i])} ph-st | {int(beam_two.elevation[i])} el')
+
+        #the boresight relative pointing is always in radians
+        G0 = beam_two.gain_tf(theta=0, phi=np.radians(beam_two.phase_steering[i]), ind=ind)
+        print(f'Pointing: az={beam_two.azimuth[j]} deg, el={beam_two.elevation[j]} deg, phase steering={beam_two.phase_steering[i]} deg -> Peak gain = {np.log10(G0)*10} dB')
+
+        axes[i,j].set_title(f'{int(beam_two.phase_steering[i])} ph-st | {int(beam_two.elevation[j])} el')
 
 pyant.plotting.show()

pyant/finite_cylindrical_parabola.py
@@ -31,10 +31,10 @@ class FiniteCylindricalParabola(Beam):
         self.height = height
         self.rotation = rotation
 
-    def normalize(self, wavelength):
+    def normalize(self, width, height, wavelength):
         '''Calculate normalization constant for beam pattern by assuming width and height >> wavelength.
         '''
-        return 4*np.pi*self.width*self.height/wavelength**2
+        return 4*np.pi*width*height/wavelength**2
 
 
     def copy(self):
@@ -94,7 +94,7 @@ class FiniteCylindricalParabola(Beam):
         wavelength = scipy.constants.c/frequency
 
         if self.I0 is None:
-            I0 = self.normalize(wavelength)
+            I0 = self.normalize(self.width, self.height, wavelength)
         else:
             I0 = self.I0
 

pyant/phased_finite_cylindrical_parabola.py
@@ -58,16 +58,16 @@ class PhasedFiniteCylindricalParabola(FiniteCylindricalParabola):
         if not self.radians:
             phase_steering = np.radians(phase_steering)
 
+        width = self.width - self.depth*np.tan(np.abs(phi)) #depth effective area loss
+        height = self.height 
+
         if self.I0 is None:
-            I0 = self.normalize(wavelength)
+            I0 = self.normalize(width, height, wavelength)
         else:
             I0 = self.I0
 
-        height = self.height 
-        width = self.width - self.depth*np.tan(np.abs(phi)) #depth effective area loss
         # x = longitudinal angle (i.e. parallel to el.axis), 0 = boresight, radians
         # y = transverse angle, 0 = boresight, radians
-
         x = width/wavelength*np.sin(phi - phase_steering)  # sinc component (longitudinal)
         y = height/wavelength*np.sin(theta)   # sinc component (transverse)
         G = np.sinc(x)*np.sinc(y) # sinc fn. (= field), NB: np.sinc includes pi !!