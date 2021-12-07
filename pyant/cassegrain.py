11f9e1a8336e7cb1c888b0aff1b923eee1ec942e
Tom Grydeland <tom.grydeland@gmail.com>
2021-12-07 15:25:15 +0000

    cleaned up code and docstrings for Cassegrain antenna

--- pyant/cassegrain.py
+++ pyant/cassegrain.py
@@ -10,14 +10,13 @@ from . import coordinates
 class Cassegrain(Beam):
     '''Cassegrain gain model of a radar dish.
 
-
     :param float I0: Peak gain (linear scale) in the pointing direction.
-    :param float a0: Radius longitudinal direction
-    :param float a1: Radius latitudinal direction
+    :param float a0: outer radius (main reflector)
+    :param float a1: inner radius (subreflector)
 
-    :ivar float I0: Peak gain (linear scale) in the pointing direction.
-    :ivar float a0: Radius longitudinal direction
-    :ivar float a1: Radius latitudinal direction
+    The gain pattern is expressed as the Fourier transform of an annular region
+    with outer radius a0 and inner radius a1.  Values below the aperture plane
+    or below the horizon should not be believed.
     '''
     def __init__(self, azimuth, elevation, frequency, I0, a0, a1, **kwargs):
         super().__init__(azimuth, elevation, frequency, **kwargs)
@@ -47,23 +46,33 @@ class Cassegrain(Beam):
         theta = coordinates.vector_angle(pointing, k, radians=True)
 
         lam = scipy.constants.c/frequency
-        k_n=2.0*np.pi/lam
+
+        eps = 1e-6
 
         if len(k.shape) == 1:
             theta = np.array([theta], dtype=k.dtype)
 
         G = np.empty((len(theta),), dtype=k.dtype)
-        inds_ = np.pi*np.sin(theta) < 1e-9
-        not_inds_ = np.logical_not(inds_)
+        nz_idx_ = np.pi*np.sin(theta) > 1e-9    # pointings not close to zero off-axis angle
+        nzth = theta[nz_idx_]
 
-        G[inds_] = self.I0
+        a0, a1, I0 = self.a0, self.a1, self.I0
 
-        A=(self.I0*((lam/(np.pi*np.sin(theta[not_inds_])))**2.0))/((self.a0**2.0-self.a1**2.0)**2.0)
-        B=(self.a0*scipy.special.jn(1,self.a0*np.pi*np.sin(theta[not_inds_])/lam)-self.a1*scipy.special.jn(1,self.a1*np.pi*np.sin(theta[not_inds_])/lam))**2.0
-        A0=(self.I0*((lam/(np.pi*np.sin(1e-6)))**2.0))/((self.a0**2.0-self.a1**2.0)**2.0)
-        B0=(self.a0*scipy.special.jn(1,self.a0*np.pi*np.sin(1e-6)/lam)-self.a1*scipy.special.jn(1,self.a1*np.pi*np.sin(1e-6)/lam))**2.0
-        const=self.I0/(A0*B0)
-        G[not_inds_] = A*B*const
+        G[:] = I0                          # will overwrite for nonzero pointings below
+
+        def scale(th):
+            return lam / (np.pi * np.sin(th))
+
+        def J1(x):
+            return scipy.special.jn(1, x)
+
+        def A(th):
+            return (scale(th) / (a0**2 - a1**2))**2
+
+        def B(th):
+            return (a0 * J1(a0/scale(th)) - a1 * J1(a1 / scale(th)))**2
+
+        G[nz_idx_] = self.I0 * (A(nzth)*B(nzth)) / (A(eps)*B(eps))
 
         if len(k.shape) == 1:
             G = G[0]
