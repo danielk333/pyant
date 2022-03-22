7c9c6aa81927a0dad55b892d80581c44a56eae77
danielk <daniel.kastinen@irf.se>
2022-03-22 15:46:21 +0100

    changed to setup.cfg format

---
+++ README.md
@@ -0,0 +1,41 @@
+# PyAnt
+
+PyAnt is a standardization for implementing radar gain pattern in Python. Nothing more, nothing less.
+
+## Getting started
+
+To install (Not yet available)
+
+```bash
+    pip install pyant
+```
+
+or 
+
+```bash
+    git clone git@github.com:danielk333/pyant.git
+    cd pyant
+    pip install .
+```
+
+Then get started by looking at the examples gallery and API in the Documentation.
+
+## Example
+
+Plot the gain pattern of a dish radar modeled using the Cassegrain model.
+
+```python
+import pyant
+
+ant = pyant.Cassegrain(
+    azimuth=0,
+    elevation=90.0, 
+    frequency=930e6,
+    I0=10**4.81,
+    a0=23.0,
+    a1=40.0,
+)
+
+pyant.plotting.gain_heatmap(ant, resolution=301, min_elevation=80.0)
+pyant.plotting.show()
+```
--- setup.py
+++ setup.py
@@ -1,35 +1,3 @@
 import setuptools
 
-__version__ = '0.8.5'
-
-with open('README.rst', 'r') as fh:
-    long_description = fh.read()
-
-
-with open('requirements', 'r') as fh:
-    pip_req = fh.read().split('\n')
-    pip_req = [x.strip() for x in pip_req if len(x.strip()) > 0]
-
-
-setuptools.setup(
-    name='pyant',
-    version=__version__,
-    long_description=long_description,
-    url='https://github.com/danielk333/pyant',
-    classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT',
-        'Operating System :: OS Independent',
-    ],
-    install_requires=pip_req,
-    packages=setuptools.find_packages(),
-    package_data={
-        'pyant.instances': ['data/*'],
-    },
-    extras_require={'plotting': ['matplotlib>=3.2.0']},
-    # metadata to display on PyPI
-    author='Daniel Kastinen',
-    author_email='daniel.kastinen@irf.se',
-    description='pyant',
-    license='MIT',
-)
+setuptools.setup()
