3c27677b91a5955b692095d05522945ccd08f78a
danielk <daniel.kastinen@irf.se>
2022-04-08 16:53:14 +0200

    moved to a src dir structure

--- setup.py
+++ setup.py
@@ -1,3 +1,16 @@
 import setuptools
+from distutils.core import Extension
 
-setuptools.setup()
+libbeam = Extension(
+    name='pyant.libbeam',
+    sources=['src/libbeam/libarray.c'],
+    include_dirs=['src/libbeam/'],
+)
+
+setuptools.setup(
+    package_dir={
+        "": "src"
+    },
+    packages=setuptools.find_packages(where="src"),
+    ext_modules=[libbeam],
+)
