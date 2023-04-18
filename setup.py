fdea9b317aade20bc44e1bb00b6cff410fafe0a8
danielk <daniel.kastinen@irf.se>
2023-04-18 16:08:42 +0200

    changed setup to single source version

--- setup.py
+++ setup.py
@@ -1,5 +1,20 @@
 import setuptools
 from distutils.core import Extension
+import pathlib
+import codecs
+
+HERE = pathlib.Path(__file__).resolve().parents[0]
+
+
+def get_version(path):
+    with codecs.open(path, 'r') as fp:
+        for line in fp.read().splitlines():
+            if line.startswith('__version__'):
+                delim = '"' if '"' in line else "'"
+                return line.split(delim)[1]
+        else:
+            raise RuntimeError("Unable to find version string.")
+
 
 libbeam = Extension(
     name='pyant.libbeam',
@@ -10,7 +25,9 @@ libbeam = Extension(
     include_dirs=['src/libbeam/'],
 )
 
+
 setuptools.setup(
+    version=get_version(HERE / 'src' / 'pyant' / 'version.py'),
     package_dir={
         "": "src"
     },
