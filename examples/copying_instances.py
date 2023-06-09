4aaf7f8db0d0333acd2384b821b0d0392925b549
danielk <daniel.kastinen@irf.se>
2023-06-09 13:43:04 +0200

    copy example

--- examples/copying_instances.py
+++ examples/copying_instances.py
@@ -1,11 +1,10 @@
-'''
+"""
 Copy of beams
-========================
-'''
+==============
+"""
+import matplotlib.pyplot as plt
 
 import pyant
-
-import numpy as np
 
 beam = pyant.models.Airy(
     azimuth=0,
@@ -13,6 +12,7 @@ beam = pyant.models.Airy(
     frequency=[930e6, 220e6],
     I0=10**4.81,
     radius=23.0,
+    degrees=True,
 )
 
 beam_2 = beam.copy()
@@ -23,32 +23,10 @@ beam_2.sph_point(
 )
 beam_2.frequency = beam_2.frequency[1:]
 
-pyant.plotting.gain_heatmap(beam, resolution=301, min_elevation=80.0)
-pyant.plotting.gain_heatmap(beam_2, resolution=301, min_elevation=80.0)
-pyant.plotting.show()
 
-xv, yv = np.meshgrid(np.linspace(-50, 50, num=22),
-                     np.linspace(-50, 50, num=22))
-antennas = np.zeros((3, 22**2))
-antennas[0, :] = xv.flatten()
-antennas[1, :] = yv.flatten()
+fig, (ax1, ax2) = plt.subplots(1, 2)
 
-arr = pyant.models.Array(
-    azimuth=0,
-    elevation=90.0,
-    frequency=46.5e6,
-    antennas=antennas,
-)
+pyant.plotting.gain_heatmap(beam, ind=(0, 0, 0), resolution=301, min_elevation=80.0, ax=ax1)
+pyant.plotting.gain_heatmap(beam_2, ind=(0, 0, 0), resolution=301, min_elevation=80.0, ax=ax2)
 
-arr2 = arr.copy()
-arr2.antennas[:100, 1] += 25
-arr2.antennas[:100, 0] -= 25
-
-arr.sph_point(
-    azimuth=0,
-    elevation=80,
-)
-
-pyant.plotting.gain_heatmap(arr, resolution=301, min_elevation=80.0)
-pyant.plotting.gain_heatmap(arr2, resolution=301, min_elevation=80.0)
-pyant.plotting.show()
+plt.show()
