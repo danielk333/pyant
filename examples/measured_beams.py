cc45f6f41a8b8571fd5cd90d7423cf071067426e
danielk <daniel.kastinen@irf.se>
2023-06-12 17:46:24 +0200

    updated uhf example

--- examples/measured_beams.py
+++ examples/measured_beams.py
@@ -11,4 +11,5 @@ fig, (ax1, ax2) = plt.subplots(1, 2)
 
 pyant.plotting.gain_heatmap(beam, min_elevation=80, ax=ax1)
 pyant.plotting.gains(beam, min_elevation=80, ax=ax2)
-pyant.plotting.show()
+
+plt.show()
