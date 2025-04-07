03f0e284e3dec46d7e7b1239f1eee37439b38140
danielk <daniel.kastinen@irf.se>
2025-04-07 16:23:16 +0200

    cleaned up install config

--- setup.py
+++ setup.py
@@ -1,32 +1,15 @@
-import setuptools
-from distutils.core import Extension
-import pathlib
-import codecs
-
-HERE = pathlib.Path(__file__).resolve().parents[0]
-
-
-def get_version(path):
-    with codecs.open(path, 'r') as fp:
-        for line in fp.read().splitlines():
-            if line.startswith('__version__'):
-                delim = '"' if '"' in line else "'"
-                return line.split(delim)[1]
-        else:
-            raise RuntimeError("Unable to find version string.")
-
+from setuptools import Extension, setup
 
 clib_beam = Extension(
-    name='pyant.clibbeam',
+    name="pyant.clibbeam",
     sources=[
-        'src/clibbeam/array.c',
-        'src/clibbeam/beam.c',
+        "src/clibbeam/array.c",
+        "src/clibbeam/beam.c",
     ],
-    include_dirs=['src/clibbeam/'],
+    include_dirs=["src/clibbeam/"],
 )
 
 
-setuptools.setup(
-    version=get_version(HERE / 'src' / 'pyant' / 'version.py'),
+setup(
     ext_modules=[clib_beam],
 )
