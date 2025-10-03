e9053c2b0542a7f63d48140807378ac2f6ca8e7b
Daniel Kastinen <daniel.kastinen@irf.se>
2025-10-03 23:32:46 +0200

    maybe matplotlib is not req but optional?

--- src/pyant/__init__.py
+++ src/pyant/__init__.py
@@ -7,27 +7,18 @@
 from .version import __version__
 
 # Modules
-from . import plotting
+try:
+    from . import plotting
+except ImportError:
+    pass
+
 from . import coordinates
-from . import beams
 from . import models
 from . import statistics
 from . import clib
 
 # Classes
 from .beam import Beam
-from .beams import Radars, Models
-
-# Functions
-from .beams import (
-    beam_of_radar,
-    avalible_beams,
-    parameters_of_radar,
-    avalible_radar_info,
-)
 
 # Static
-from .beams import UNITS
-
-# Profiling
-from .profiling import profile, profile_stop, get_profile, print_profile, profile_clear
+from .radars import UNITS, RADAR_PARAMETERS
