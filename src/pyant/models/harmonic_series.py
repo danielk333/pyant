465e3e78ebff346817a6c46800449d7eaad63bbb
danielk <daniel.kastinen@irf.se>
2025-10-09 16:10:26 +0200

    proper todos

--- src/pyant/models/harmonic_series.py
+++ src/pyant/models/harmonic_series.py
@@ -1,15 +1,6 @@
-#!/usr/bin/env python
-
-import copy
-
-import numpy as np
-import scipy.constants
-import scipy.special
-
 from ..beam import Beam
-from .. import coordinates
 
 
-class Isotropic(Beam):
+class HarmonicSeries(Beam):
     def __init__(self):
         raise NotImplementedError("todo")
