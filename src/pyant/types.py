1131516dd36322ac89349ef6d92e8ff50aec6b01
danielk <daniel.kastinen@irf.se>
2025-10-09 15:43:17 +0200

    start of typing the lib

---
+++ src/pyant/types.py
@@ -0,0 +1,17 @@
+"""
+This module contains convenient type information so that typing can be precise
+but not too verbose in the code itself.
+"""
+
+import numpy.typing as npt
+
+NDArray = npt.NDArray
+
+NDArray_N = npt.NDArray
+"(n,) shaped ndarray"
+
+NDArray_3 = npt.NDArray
+"(3,) shaped ndarray"
+
+NDArray_3xN = npt.NDArray
+"(3,n) shaped ndarray"
