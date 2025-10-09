abaf281b28fe78135dd37b46fbd299a490570339
danielk <daniel.kastinen@irf.se>
2025-10-09 15:42:21 +0200

    move useful coordinate related functions to proper module

--- src/pyant/coordinates.py
+++ src/pyant/coordinates.py
@@ -8,6 +8,64 @@ CLOSE_TO_POLE_LIMIT = 1e-9**2
 CLOSE_TO_POLE_LIMIT_rad = np.arctan(1 / np.sqrt(CLOSE_TO_POLE_LIMIT))
 
 
+def _clint(p, c, lim=1):
+    """clip interval [p-c, p+c] to [-lim, lim] (lim=1 by default)"""
+    return np.clip([p - c, p + c], -lim, lim)
+
+
+def compute_j_grid(resolution):
+    """Compute a grid of polarizations with given resolution
+    """
+    size = resolution**2
+    thx = np.linspace(0, 2 * np.pi, num=resolution)
+    thy = np.linspace(0, 2 * np.pi, num=resolution)
+
+    jones_vecs = np.zeros((2, size), dtype=np.complex128)
+
+    thxmat, thymat = np.meshgrid(thx, thy, sparse=False, indexing="ij")
+
+    jones_vecs[0, :] = np.exp(1j * thxmat.reshape(1, size))
+    jones_vecs[1, :] = np.exp(1j * thymat.reshape(1, size))
+
+    return jones_vecs, thxmat, thymat
+
+
+def compute_k_grid(pointing, resolution, centered, cmin):
+    """Compute a grid of wave vector directions with given resolution
+    """
+    if centered:
+        kx = np.linspace(*_clint(pointing[0], cmin), num=resolution)
+        ky = np.linspace(*_clint(pointing[1], cmin), num=resolution)
+    else:
+        kx = np.linspace(-cmin, cmin, num=resolution)
+        ky = np.linspace(-cmin, cmin, num=resolution)
+
+    K = np.zeros((resolution, resolution, 2))
+
+    # TODO: Refactor evaluation of function on a hemispherical domain to a function"
+    K[:, :, 0], K[:, :, 1] = np.meshgrid(kx, ky, sparse=False, indexing="ij")
+    size = resolution**2
+    k = np.empty((3, size), dtype=np.float64)
+    k[0, :] = K[:, :, 0].reshape(1, size)
+    k[1, :] = K[:, :, 1].reshape(1, size)
+
+    # circles in k space, centered on vertical and pointing, respectively
+    z2 = k[0, :] ** 2 + k[1, :] ** 2
+    z2_c = (pointing[0] - k[0, :]) ** 2 + (pointing[1] - k[1, :]) ** 2
+
+    if centered:
+        inds = np.logical_and(z2_c < cmin**2, z2 <= 1.0)
+    else:
+        inds = z2 < cmin**2
+    not_inds = np.logical_not(inds)
+
+    k[2, inds] = np.sqrt(1.0 - z2[inds])
+    k[2, not_inds] = 0
+    S = np.ones((size,)) * np.nan
+
+    return S, K, k, inds, kx, ky
+
+
 def cart_to_sph(vec, degrees=False):
     """Convert from Cartesian coordinates (east, north, up) to Spherical
     coordinates (azimuth, elevation, range) in a angle east of north and
--- src/pyant/plotting.py
+++ src/pyant/plotting.py
@@ -13,63 +13,9 @@ from .beam import Beam
 from . import coordinates as coord
 
 
-def _clint(p, c, lim=1):
-    """clip interval [p-c, p+c] to [-lim, lim] (lim=1 by default)"""
-    return np.clip([p - c, p + c], -lim, lim)
-
-
 def add_circle(ax, c, r, fmt="k--", *args, **kw):
     th = np.linspace(0, 2 * np.pi, 180)
     ax.plot(c[0] + np.cos(th), c[1] + np.sin(th), fmt, *args, **kw)
-
-
-def compute_j_grid(resolution):
-    size = resolution**2
-    thx = np.linspace(0, 2 * np.pi, num=resolution)
-    thy = np.linspace(0, 2 * np.pi, num=resolution)
-
-    jones_vecs = np.zeros((2, size), dtype=np.complex128)
-
-    thxmat, thymat = np.meshgrid(thx, thy, sparse=False, indexing="ij")
-
-    jones_vecs[0, :] = np.exp(1j * thxmat.reshape(1, size))
-    jones_vecs[1, :] = np.exp(1j * thymat.reshape(1, size))
-
-    return jones_vecs, thxmat, thymat
-
-
-def compute_k_grid(pointing, resolution, centered, cmin):
-    if centered:
-        kx = np.linspace(*_clint(pointing[0], cmin), num=resolution)
-        ky = np.linspace(*_clint(pointing[1], cmin), num=resolution)
-    else:
-        kx = np.linspace(-cmin, cmin, num=resolution)
-        ky = np.linspace(-cmin, cmin, num=resolution)
-
-    K = np.zeros((resolution, resolution, 2))
-
-    # TODO: Refactor evaluation of function on a hemispherical domain to a function"
-    K[:, :, 0], K[:, :, 1] = np.meshgrid(kx, ky, sparse=False, indexing="ij")
-    size = resolution**2
-    k = np.empty((3, size), dtype=np.float64)
-    k[0, :] = K[:, :, 0].reshape(1, size)
-    k[1, :] = K[:, :, 1].reshape(1, size)
-
-    # circles in k space, centered on vertical and pointing, respectively
-    z2 = k[0, :] ** 2 + k[1, :] ** 2
-    z2_c = (pointing[0] - k[0, :]) ** 2 + (pointing[1] - k[1, :]) ** 2
-
-    if centered:
-        inds = np.logical_and(z2_c < cmin**2, z2 <= 1.0)
-    else:
-        inds = z2 < cmin**2
-    not_inds = np.logical_not(inds)
-
-    k[2, inds] = np.sqrt(1.0 - z2[inds])
-    k[2, not_inds] = 0
-    S = np.ones((size,)) * np.nan
-
-    return S, K, k, inds, kx, ky
 
 
 def antenna_configuration(antennas, ax=None, color=None, z_axis=True):
@@ -252,7 +198,7 @@ def gain_surface(
         pointing = beam.parameters["pointing"]
 
     cmin = np.cos(np.radians(min_elevation))
-    S, K, k, inds, kx, ky = compute_k_grid(pointing, resolution, centered, cmin)
+    S, K, k, inds, kx, ky = coord.compute_k_grid(pointing, resolution, centered, cmin)
 
     S[inds] = beam.gain(k[:, inds], polarization=polarization)
     S = S.reshape(resolution, resolution)
@@ -317,7 +263,7 @@ def polarization_heatmap(
         )
 
     # We will draw a k-space circle centered on `pointing` with a radius of cos(min_elevation)
-    jones_vecs, thxmat, thymat = compute_j_grid(resolution)
+    jones_vecs, thxmat, thymat = coord.compute_j_grid(resolution)
 
     g = np.zeros((jones_vecs.shape[1],), dtype=np.float64)
     for ind in range(jones_vecs.shape[1]):
@@ -419,7 +365,7 @@ def gain_heatmap(
 
     # We will draw a k-space circle centered on `pointing` with a radius of cos(min_elevation)
     cmin = np.cos(np.radians(min_elevation))
-    S, K, k, inds, kx, ky = compute_k_grid(pointing, resolution, centered, cmin)
+    S, K, k, inds, kx, ky = coord.compute_k_grid(pointing, resolution, centered, cmin)
 
     S[inds] = beam.gain(k[:, inds], polarization=polarization)
     S = S.reshape(resolution, resolution)
@@ -514,7 +460,7 @@ def hemisphere_plot(
 
     # We will draw a k-space circle centered on `pointing` with a radius of cos(min_elevation)
     cmin = np.cos(np.radians(min_elevation))
-    S, K, k, inds, kx, ky = compute_k_grid(pointing, resolution, centered, cmin)
+    S, K, k, inds, kx, ky = coord.compute_k_grid(pointing, resolution, centered, cmin)
 
     S[inds] = func(k[:, inds]).flatten()
     S = S.reshape(resolution, resolution)
@@ -578,7 +524,7 @@ def gain_heatmap_movie(
     else:
         pointing = beam.parameters["pointing"]
     cmin = np.cos(np.radians(min_elevation))
-    S, K, k, inds, kx, ky = compute_k_grid(pointing, resolution, centered, cmin)
+    S, K, k, inds, kx, ky = coord.compute_k_grid(pointing, resolution, centered, cmin)
 
     def run(it, fig, ax, mesh, beam, polarization, resolution, S, k, inds):
         beam = beam_update(beam, it)
