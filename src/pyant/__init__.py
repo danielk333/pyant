a87ce25a0a2136bfda1613b58148a5d7e2168b95
danielk <daniel.kastinen@irf.se>
2025-10-09 15:42:03 +0200

    good way of handling modules with optional depenacies

--- src/pyant/__init__.py
+++ src/pyant/__init__.py
@@ -1,24 +1,29 @@
 #!/usr/bin/env python
 
-"""
-
-"""
-
+""" """
+import types
 from .version import __version__
 
 # Modules
 try:
     from . import plotting
-except ImportError:
-    pass
+except ImportError as err:
+    if err.name != "matplotlib":
+        raise
+
+    class _MissingModule(types.ModuleType):
+        def __getattr__(self, name):
+            raise ImportError(
+                "The optional dependency `matplotlib` for 'plotting' module is missing.\n"
+                "Install it with `pip install pyant[plotting]` or `pip install matplotlib`."
+            )
+
+    plotting = _MissingModule("plotting")
 
 from . import coordinates
 from . import models
 from . import statistics
 from . import clib
+from . import beams
 
-# Classes
 from .beam import Beam
-
-# Static
-from .radars import UNITS, RADAR_PARAMETERS
